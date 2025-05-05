72aff8d833a2ef56d535279d81518ebd48d5eace
CobraTEM <vladeknew@gmail.com>
2025-05-05 21:09:53 +0200

    Added Fade to bottom of video and added audio

---
+++ audio_handler.js
@@ -0,0 +1,26 @@
+window.onload = setInterval(Loop, 1000 / 10); //10fps
+window.onload = setInterval(Loop2, 1000 / 10); //10fps
+
+let HomeAudio = new Audio();
+let UpAudio = new Audio();
+
+HomeAudio.src = 'audio/Deadlock_Main_Theme.mp3';
+UpAudio.src = 'audio/music_teleporter_01.mp3';
+
+function Loop() {
+    if (document.getElementById("Home").innerHTML) {
+        HomeAudio.play();
+    if (HomeAudio.paused == true) {
+        HomeAudio.play();
+        }
+    }
+
+}
+function Loop2() {
+if (document.getElementById("Updates").innerHTML) {
+        UpAudio.play();
+    if (UpAudio.paused == true) {
+        UpAudio.play();
+        }
+    }
+}
