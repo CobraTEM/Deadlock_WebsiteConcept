6d56e44c26b8752746ed95c215a2b9fd427cae28
CobraTEM <vladeknew@gmail.com>
2025-05-12 22:47:41 +0200

    MORE CLEAN UP

--- audio_handler.js
+++ audio_handler.js
@@ -1,12 +1,22 @@
+
+import {Howl, Howler} from 'howler';
+
+const {Howl, Howler} = require('howler');
+
+var label = document.getElementById('HeroCard4');
+
 window.onload = setInterval(Loop, 1000 / 10); //10fps
 window.onload = setInterval(Loop2, 1000 / 10); //10fps
 window.onload = setInterval(Loop3, 1000 / 10); //10fps
 
 let HomeAudio = new Audio();
 let UpAudio = new Audio();
+let HoverAudio = new Audio();
+
 
 HomeAudio.src = 'audio/Deadlock_Main_Theme.mp3';
 UpAudio.src = 'audio/music_teleporter_01.mp3';
+HoverAudio.src = 'audio/ui/ui_menu_hover_01.mp3';
 
 function Loop() {
     if (document.getElementById("Home").innerHTML) {
